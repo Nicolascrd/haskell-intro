e250b2e46a08f404c7a1045a93e5a88c542cc5a9
Nicolas Couraud <nic.crd@gmail.com>
2023-03-15 17:26:45 +0100

    nicer formatting

--- 01-Intro/towersHanoi.hs
+++ 01-Intro/towersHanoi.hs
@@ -1,10 +1,10 @@
 type Peg = String
+
 type Move = (Peg, Peg)
 
-hanoi :: Integer -> Peg -> Peg -> Peg ->  [Move] 
+hanoi :: Integer -> Peg -> Peg -> Peg -> [Move]
 hanoi n p1 p2 p3
-    | n == 0 = []
-    | otherwise = (hanoi (n-1) p1 p3 p2) ++ [(p1, p2)] ++ (hanoi (n-1) p3 p2 p1)
-
+  | n == 0 = []
+  | otherwise = hanoi (n - 1) p1 p3 p2 ++ [(p1, p2)] ++ hanoi (n - 1) p3 p2 p1
 
 -- hanoi 2 "a" "b" "c" == [("a","c"), ("a","b"), ("c","b")]
