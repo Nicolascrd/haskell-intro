532032c1526672754cc424bb29dd3eb1d40061a0
Nicolas Couraud <nic.crd@gmail.com>
2023-03-16 16:08:21 +0100

    [03] remove recursions, remove edge cases -> clean

--- 03-Rec-Poly/Golf.hs
+++ 03-Rec-Poly/Golf.hs
@@ -1,14 +1,11 @@
 module Golf where
 
+-- EX 1 skips
+
 takeNthElement :: Int -> [a] -> [a]
-takeNthElement 0 _ = []
-takeNthElement n [] = []
-takeNthElement n l = takeNthElementRec n 1 l
+takeNthElement n = takeNthElementRec n 1
 
 takeNthElementRec :: Int -> Int -> [a] -> [a]
-takeNthElementRec 0 _ _ = []
-takeNthElementRec _ 0 _ = []
--- first two should not happen
 takeNthElementRec _ _ [] = []
 takeNthElementRec n c (x : xs)
   | c == n = x : takeNthElementRec n 1 xs -- take element
@@ -16,40 +13,29 @@ takeNthElementRec n c (x : xs)
 
 skips :: [a] -> [[a]]
 skips [] = []
-skips l = skipsRec 1 l
+skips l = [takeNthElement i l | i <- [1 .. n]]
+  where
+    n = length l
 
-skipsRec :: Int -> [a] -> [[a]]
-skipsRec _ [] = []
-skipsRec 0 _ = []
-skipsRec n l
-  | length l == n = [[last l]]
-  | otherwise = takeNthElement n l : skipsRec (n + 1) l
+-- EX 2 localMaxima
 
 localMaxima :: [Integer] -> [Integer]
-localMaxima [x1, x2, x3] = localMaximaTest x1 x2 x3
-localMaxima (x1 : x2 : x3 : xs) = localMaximaTest x1 x2 x3 ++ localMaxima ([x2, x3] ++ xs)
+localMaxima (x1 : x2 : x3 : xs)
+  | x1 < x2 && x2 > x3 = x2 : localMaxima ([x2, x3] ++ xs)
+  | otherwise = localMaxima ([x2, x3] ++ xs)
+localMaxima _ = []
 
-localMaximaTest :: Integer -> Integer -> Integer -> [Integer]
-localMaximaTest a b c
-  | a < b && b > c = [b]
-  | otherwise = []
-
-incremN :: [Integer] -> Integer -> [Integer]
--- input is a array of length 10 representing counters, and the one to increment
-incremN [] _ = []
-incremN (x : xs) 0 = (x + 1) : xs
-incremN (x : xs) n = x : incremN xs (n - 1)
+-- EX 3 histogram
 
 countOccurences :: [Integer] -> [Integer]
 -- count occurrences of 0s to 9s
-countOccurences l = countOccurencesRec l (take 10 [0, 0 .. 0])
+countOccurences = foldl incrementOccurence (take 10 [0, 0 .. 0])
 
-countOccurencesRec :: [Integer] -> [Integer] -> [Integer]
-countOccurencesRec [] res = res -- end of list
-countOccurencesRec (x : xs) res = countOccurencesRec xs (init start ++ [last start + 1] ++ end)
+incrementOccurence :: [Integer] -> Integer -> [Integer]
+incrementOccurence res n = init start ++ [last start + 1] ++ end
   where
-    start = take (fromIntegral x + 1) res -- last start is the one to increment
-    end = drop (fromIntegral x + 1) res
+    start = take (fromIntegral n + 1) res -- last start is the one to increment
+    end = drop (fromIntegral n + 1) res
 
 decrementOrZero :: Integer -> Integer
 decrementOrZero 0 = 0
@@ -63,5 +49,9 @@ histogram :: [Integer] -> String
 histogram l = histogramFromOccurrences (countOccurences l) ++ "==========\n0123456789"
 
 histogramFromOccurrences :: [Integer] -> String
-histogramFromOccurrences [0, 0, 0, 0, 0, 0, 0, 0, 0, 0] = ""
-histogramFromOccurrences l = histogramFromOccurrences (map decrementOrZero l) ++ map spaceOrStar l ++ "\n"
+histogramFromOccurrences l
+  | equalInitCounter l = ""
+  | otherwise = histogramFromOccurrences (map decrementOrZero l) ++ map spaceOrStar l ++ "\n"
+
+equalInitCounter :: [Integer] -> Bool
+equalInitCounter l = l == take 10 [0, 0 .. 0]
