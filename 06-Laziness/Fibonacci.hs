40a0d8052cb4ec92012ca744f50328633ceb96c7
Nicolas Couraud <sp.couraud.nicolas@gmail.com>
2023-03-21 17:31:28 +0100

    end 06 : ex 6 and 7

--- 06-Laziness/Fibonacci.hs
+++ 06-Laziness/Fibonacci.hs
@@ -1,4 +1,6 @@
 {-# LANGUAGE FlexibleInstances #-}
+{-# OPTIONS_GHC -fno-warn-missing-methods #-}
+
 -- EX 1
 
 fib :: Integer -> Integer
@@ -60,3 +62,34 @@ interleaveStreams (Stream x s) s' = Stream x (interleaveStreams s' s)
 
 newRuler :: Stream Integer
 newRuler = foldr1 interleaveStreams (streamToList (streamMap streamRepeat nats))
+
+-- EX 6
+
+x :: Stream Integer
+x = Stream 0 (Stream 1 (streamRepeat 0))
+
+instance Num (Stream Integer) where
+  fromInteger n = Stream n (streamRepeat 0)
+  negate (Stream n s) = Stream (-n) (negate s)
+  (+) (Stream n s) (Stream n' s') = Stream (n + n') (s + s')
+  (*) (Stream n s) (Stream n' s') = Stream (n * n') (streamMap (* n) s' + (s * Stream n' s'))
+
+instance Fractional (Stream Integer) where
+  (/) (Stream n s) (Stream n' s') = Stream (n `div` n') (streamMap (`div` n') (s - ((Stream n s / Stream n' s') * s')))
+
+fibs3 :: Stream Integer
+fibs3 = x / (1 - x - x * x)
+
+-- EX 7
+
+data Matrix = Matrix Integer Integer Integer Integer
+
+instance Num Matrix where
+  (*) (Matrix a b c d) (Matrix a' b' c' d') = Matrix (a * a' + b * c') (a * b' + b * d') (c * a' + d * c') (c * b' + d * d')
+
+fib4 :: Integer -> Integer
+fib4 0 = 0
+fib4 1 = 1
+fib4 n = fibFromMatrix (Matrix 1 1 1 0 ^ n)
+  where
+    fibFromMatrix (Matrix a b c d) = b
